f1c759f792a2a498632ead69f39b4586775f0c7b
Omar OUGHZAL <omar.oughzal@gmail.com>
2023-11-16 00:11:31 +0100

    initial Commit

---
+++ app/src/main/java/com/omarcomputer/bluetooth/MainActivity.java
@@ -0,0 +1,136 @@
+package com.omarcomputer.bluetooth;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.omarcomputer.bluetooth.databinding.ActivityMainBinding;
+
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityResultLauncher<Intent> enableBluetoothLauncher;
+    ActivityMainBinding binding;
+
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //TODO : configurer le launcher pour activer le bluetooth
+        enableBluetoothLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        binding.BTEnabled.setChecked(true);
+                    } else {
+                        binding.BTEnabled.setChecked(false);
+                    }
+                }
+        );
+
+        binding.btnCheckBluetooth.setOnClickListener(v -> {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.BLUETOOTH_CONNECT},
+                        1000);
+                return;
+            }
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (bluetoothAdapter != null) {
+                binding.BTAdapter.setChecked(true);
+            } else {
+                binding.BTAdapter.setChecked(false);
+            }
+
+            if (bluetoothAdapter.isEnabled()) {
+                binding.BTEnabled.setChecked(true);
+            } else {
+                binding.BTEnabled.setChecked(false);
+
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                enableBluetoothLauncher.launch(enableBtIntent);
+            }
+        });
+
+        binding.btnGetPaired.setOnClickListener(v -> {
+
+                BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (bluetoothAdapter != null && bluetoothAdapter.isEnabled()) {
+                    Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+                    if (pairedDevices.size() > 0) {
+                        String list = "";
+                        for (BluetoothDevice device : pairedDevices) {
+                            list += device.getName() + ":" + device.getAddress();
+                        }
+                        binding.txtDeviceList.setText(list);
+                    }
+
+            }
+
+        });
+
+        binding.btnDevicesDiscovry.setOnClickListener(v -> {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.BLUETOOTH_SCAN},
+                        1001);
+                return;
+            }
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        1002);
+                return;
+
+            }
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (bluetoothAdapter != null && bluetoothAdapter.isEnabled()) {
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                registerReceiver(receiver, filter);
+                bluetoothAdapter.startDiscovery();
+            }
+        });
+
+
+    }
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    String deviceName = device.getName();
+                    String macAddress = device.getAddress();
+                    String out = binding.txtDeviceFoundList.getText().toString();
+                    out += deviceName + ": " + macAddress;
+                    binding.txtDeviceFoundList.setText(out);
+                }
+
+            }
+        }
+    };
+}
